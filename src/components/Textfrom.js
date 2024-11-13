1417e5a439c111fe9ca074d0ab3e8e0af7374c95
Sagarraj2001 <sagarraj19042001@gmail.com>
2024-11-13 10:24:18 +0530

    first commit

--- src/components/Textfrom.js
+++ src/components/Textfrom.js
@@ -48,20 +48,20 @@ export default function Textfrom(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" id="myBox" style={{backgroundColor:props.mode==='dark'?'white':'#042743',color:props.mode==='dark'?'#042743':'white'}} rows="8" value={text} onChange={handleonchange}></textarea>
-                <button className="btn btn-primary my-3 mx-2" onClick={changeUppercase} >UPPERCASE</button>
-                <button className="btn btn-danger my-3 mx-2"  onClick={changeLowercase} >lowercase</button>
-                <button className="btn btn-warning my-3 mx-2" onClick={changeclear} >Clear Text</button>
-                <button className="btn btn-success my-3 mx-2" onClick={Capitalizedcase} >Capitalized Case</button>
-                <button className="btn btn-danger my-3 mx-2" onClick={SentenceCase} >Sentence Case</button>
-                <button className="btn btn-warning my-3 mx-2" onClick={copyText} >copy Text</button>
-                <button className="btn btn-success my-3 mx-2" onClick={extraSpaces} >remove Extraspaces</button>
+                <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={changeUppercase} >UPPERCASE</button>
+                <button disabled={text.length===0} className="btn btn-danger my-3 mx-2"  onClick={changeLowercase} >lowercase</button>
+                <button disabled={text.length===0} className="btn btn-warning my-3 mx-2" onClick={changeclear} >Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-success my-3 mx-2" onClick={Capitalizedcase} >Capitalized Case</button>
+                <button disabled={text.length===0} className="btn btn-danger my-3 mx-2" onClick={SentenceCase} >Sentence Case</button>
+                <button disabled={text.length===0} className="btn btn-warning my-3 mx-2" onClick={copyText} >copy Text</button>
+                <button disabled={text.length===0} className="btn btn-success my-3 mx-2" onClick={extraSpaces} >remove Extraspaces</button>
 
             </div>
         </div>
         <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>
             <h1>your text summary</h1>
-            <p>Character count: {text.length} | Word count: {text.split(" ").length} | Line Count: {text.split(/\n/).length}</p>
-            <p>{text.split(" ").length * 0.008} Minutes read</p>
+            <p>Character count: {text.length} | Word count: {text.split(" ").filter((element)=>{return element.length !== 0 }).length} | Line Count: {text.split(/\n/).length}</p>
+            <p>{text.split(" ").filter((element)=>{return element.length !== 0}).length * 0.008} Minutes read</p>
             <h2>text preview</h2>
             <p>{text.length>0?text:'Enter something in text box for preview.'}</p>
         </div>
